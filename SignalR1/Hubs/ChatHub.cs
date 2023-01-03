243827f9e0afbeca0107ce215efd62dc5e89d922
efraimt <efraimt@gmail.com>
2023-01-03 14:06:58 +0200

    Add project files.

---
+++ SignalR1/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR1.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+
+        public class ChatHub : Hub
+        {
+            public async Task SendMessage(string user, string message)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", user, message);
+            }
+        
+    }
+}
